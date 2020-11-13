969e76b1894ccf0d1e85afef49b6e856e2e0678f
Andreas Mellberg <andreas.mellberg@gmail.com>
2020-11-13 17:40:33 +0100

    Minor stuff

--- day06/part2.js
+++ day06/part2.js
@@ -1,31 +1,31 @@
 const { readFileSync } = require("fs");
 
 function findPath(orbitMap, start, end) {
-    // Setup some attributes for each object (vertex)
+    // Set up some attributes for each object (vertex)
     const attrs = Object.create(null);
     for (const obj of Object.keys(orbitMap)) {
-        attrs[obj] = { prev: null, visited: false };
+        attrs[obj] = { parent: null, visited: false };
     }
 
-    // Run a DFS from start, looking for end, storing prev pointers as we go
+    // Run a DFS from start, looking for end, storing parent pointers as we go
     (function find(currObj) {
         attrs[currObj].visited = true;
         if (currObj === end) return true;
         for (const obj of orbitMap[currObj]) {
             if (!attrs[obj].visited) {
-                attrs[obj].prev = currObj;
+                attrs[obj].parent = currObj;
                 if (find(obj)) return true;
             }
         }
         return false;
     })(start);
 
-    // Trace out a path in reverse, from end to start, using the prev pointers
+    // Trace out a path in reverse, from end to start, using the parent pointers
     const path = [];
-    let currObj = attrs[end].prev;
+    let currObj = attrs[end].parent;
     while (currObj !== start) {
         path.push(currObj);
-        currObj = attrs[currObj].prev;
+        currObj = attrs[currObj].parent;
     }
     return path;
 }
@@ -50,12 +50,6 @@ function main(inputFile) {
     console.log(path.length - 1);
 }
 
-const data =
-    "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN";
-const map = createOrbitMap(data);
-
 if (require.main === module) {
     main("input.txt");
 }
-
-module.exports = {};
